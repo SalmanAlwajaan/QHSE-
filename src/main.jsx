bf633df95e242495cbf94a885aed94a2fe16c04f
SalmanAlwajaan <salman.k.alwajaan@gmail.com>
2026-07-19 04:04:11 +0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './i18n/index.js'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(<App />)
